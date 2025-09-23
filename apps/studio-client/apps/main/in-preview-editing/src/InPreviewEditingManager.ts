e6ef5e2315241b987dd136c2d4a7549b80902ded
Sebastian Büttner <sebastian.buettner@coremedia.com>
2025-09-23 14:30:38 +0200

    Adding breadcrumb and metrics, use master for translation workflow dialog

--- apps/studio-client/apps/main/in-preview-editing/src/InPreviewEditingManager.ts
+++ apps/studio-client/apps/main/in-preview-editing/src/InPreviewEditingManager.ts
@@ -20,8 +20,14 @@ import ShowStartTranslationWorkflowWindowAction from "@coremedia/studio-client.m
 import editorContext from "@coremedia/studio-client.main.editor-components/sdk/editorContext";
 import EditorContextImpl from "@coremedia/studio-client.main.editor-components/sdk/EditorContextImpl";
 import GlobalShowStartPublicationWorkflowWindowAction from "@coremedia/studio-client.main.control-room-editor-components/actions/GlobalShowStartPublicationWorkflowWindowAction";
-import FloatingEditorDialog from "./editors/FloatingEditorDialog";
+import OpenDialogAction from "@coremedia/studio-client.ext.ui-components/actions/OpenDialogAction";
+import Config from "@jangaroo/runtime/Config";
+import CreateFromTemplateDialog from "@coremedia-blueprint/studio-client.main.create-from-template-studio-plugin/CreateFromTemplateDialog";
+import TranslationStatusUtil from "@coremedia/studio-client.ext.workflow-components/components/form/translation/TranslationStatusUtil";
+import { sitesService, SiteUtil } from "@coremedia/studio-client.multi-site-models";
+import { fetchFromRemoteService } from "@coremedia/studio-client.client-core";
 import InPreviewEditingUtil from "./utils/InPreviewEditingUtil";
+import FloatingEditorDialog from "./editors/FloatingEditorDialog";
 
 class InPreviewEditingManager {
   #previewIframe: PreviewIFrame = null;
@@ -29,11 +35,14 @@ class InPreviewEditingManager {
 
   static readonly MESSAGE_TYPE_CONTENT_METADATA_REQUEST: string = "com.coremedia.pde.content.metadata.request";
   static readonly MESSAGE_TYPE_CONTENT_METADATA_RESPONSE: string = "com.coremedia.pde.content.metadata.response";
+  static readonly MESSAGE_TYPE_CONTENT_METRICS_REQUEST: string = "com.coremedia.pde.content.metrics.request";
+  static readonly MESSAGE_TYPE_CONTENT_METRICS_RESPONSE: string = "com.coremedia.pde.content.metrics.response";
   static readonly MESSAGE_TYPE_PROPERTY_UPDATE: string = "com.coremedia.pde.propertyUpdate";
   static readonly MESSAGE_TYPE_SHOW_EDITOR: string = "com.coremedia.pde.showEditor";
   static readonly MESSAGE_TYPE_OPEN_CONTENT: string = "com.coremedia.pde.openContent";
   static readonly MESSAGE_TYPE_SHOW_IN_LIBRARY: string = "com.coremedia.pde.showInLibrary";
   static readonly MESSAGE_TYPE_OPEN_NAVIGATION_MANAGER: string = "com.coremedia.pde.openNavigationManager";
+  static readonly MESSAGE_TYPE_CREATE_PAGE_FROM_TEMPLATE: string = "com.coremedia.pde.createPageFromTemplate";
   static readonly MESSAGE_TYPE_START_LOCALIZATION: string = "com.coremedia.pde.startLocalization";
   static readonly MESSAGE_TYPE_START_PUBLICATION: string = "com.coremedia.pde.startPublication";
   static readonly MESSAGE_TYPE_PUBLISH_REQUEST: string = "com.coremedia.pde.content.publish.request";
@@ -78,6 +87,13 @@ class InPreviewEditingManager {
           bind(this, this.#contentMetadataListener),
         );
 
+        // register content metrics listener
+        messageService.registerMessageListener(
+          iframeEl,
+          InPreviewEditingManager.MESSAGE_TYPE_CONTENT_METRICS_REQUEST,
+          bind(this, this.#contentMetricsListener),
+        );
+
         // register open content listener
         messageService.registerMessageListener(
           iframeEl,
@@ -97,6 +113,13 @@ class InPreviewEditingManager {
           iframeEl,
           InPreviewEditingManager.MESSAGE_TYPE_OPEN_NAVIGATION_MANAGER,
           bind(this, this.#openNavigationManagerListener),
+        );
+
+        // register create page from template listener
+        messageService.registerMessageListener(
+          iframeEl,
+          InPreviewEditingManager.MESSAGE_TYPE_CREATE_PAGE_FROM_TEMPLATE,
+          bind(this, this.#openCreatePageFromTemplateWizardListener),
         );
 
         // register start localization workflow listener
@@ -194,8 +217,20 @@ class InPreviewEditingManager {
     InPreviewEditingUtil.openNavigationManager();
   }
 
+  #openCreatePageFromTemplateWizardListener() {
+    console.log("[InPreviewEditingManager] Open create page from template wizard ...");
+    new OpenDialogAction({
+      dialogDefaults: Config(CreateFromTemplateDialog, {}),
+    }).execute();
+  }
+
   #openLocalizationWorkflowDialogListener(event: { contentRef: string }) {
-    const content = session._.getConnection().getContentRepository().getContent(event.contentRef);
+    let content = session._.getConnection().getContentRepository().getContent(event.contentRef);
+    const master = sitesService._.getMaster(content);
+    if (master) {
+      content = master;
+    }
+
     const processDefinitions = cast(EditorContextImpl, editorContext._).getTranslationProcessDefinitions();
     const action = new ShowStartTranslationWorkflowWindowAction({
       contentValueExpression: ValueExpressionFactory.createFromValue(content),
@@ -213,16 +248,28 @@ class InPreviewEditingManager {
     action.execute();
   }
 
-  #contentMetadataListener(event: { contentRef: string; propertyName: string }): void {
+  #contentMetadataListener(event: { contentRef: string; propertyName: string; breadcrumbIds: string[] }): void {
     console.log("[InPreviewEditingManager] Received content metadata request event: ", event);
-    this.#calculateContentMetadata(event.contentRef, event.propertyName)
+    this.#calculateContentMetadata(event.contentRef, event.propertyName, event.breadcrumbIds)
       .then(bind(this, this.#sendContentMetadataResponse))
       .catch((e) => {
         console.log("[InPreviewEditingManager] Error while loading content metadata: ", e);
       });
   }
 
-  #calculateContentMetadata(contentRef: string, propertyName: string) {
+  #contentMetricsListener(event: { contentRef }): void {
+    console.log("[InPreviewEditingManager] Received content metrics request event: ", event);
+
+    const content = as(session._.getConnection().getContentRepository().getContent(event.contentRef), Content);
+    this.#loadMetrics(content)
+      .then(bind(this, this.#sendContentMetricsResponse))
+      .catch((e) => {
+        console.log("[InPreviewEditingManager] Error while loading content metrics: ", e);
+        this.#sendContentMetricsResponse({ metrics: null });
+      });
+  }
+
+  #calculateContentMetadata(contentRef: string, propertyName: string, breadcrumbIds: string[]) {
     return new Promise<unknown>((resolve, reject) => {
       const content = session._.getConnection().getContentRepository().getContent(contentRef);
       if (!content || !propertyName) {
@@ -233,9 +280,12 @@ class InPreviewEditingManager {
         this.#loadContentName(content),
         this.#loadContentTypeName(content),
         this.#loadContentStatus(content),
+        this.#loadSiteLocale(content),
+        this.#loadTranslationStatus(content),
         this.#loadUserMayPerformPublish(content),
         this.#loadContentThumbnail(content),
         this.#loadPropertyMetadata(content, propertyName),
+        this.#loadBreadcrumb(content, breadcrumbIds),
       ])
         .then((results) => {
           const metadata = Object.assign({ contentRef: contentRef, contentId: content.getId() }, ...results);
@@ -270,7 +320,7 @@ class InPreviewEditingManager {
             toastService._.showToast(title, text, ValidationState.ERROR);
           }
 
-          this.#calculateContentMetadata(event.contentRef, event.propertyName)
+          this.#calculateContentMetadata(event.contentRef, event.propertyName, [])
             .then(bind(this, this.#sendContentMetadataResponse))
             .catch((e) => {
               console.log("[InPreviewEditingManager] Error while loading content metadata: ", e);
@@ -291,6 +341,33 @@ class InPreviewEditingManager {
           resolve({ contentName: name });
         });
       }
+    });
+  }
+
+  #loadBreadcrumb(content: Content, breadcrumbIds: string[]) {
+    return new Promise<unknown>((resolve, reject) => {
+      const breadcrumbLoaders = breadcrumbIds.map((contentRef) => {
+        return new Promise((resolve, reject) => {
+          const content = session._.getConnection().getContentRepository().getContent(contentRef);
+          if (!content) {
+            reject();
+          }
+          Promise.all([this.#loadContentName(content), this.#loadContentTypeName(content)])
+            .then((results) => {
+              const metadata = Object.assign({ contentRef: contentRef, contentId: content.getId() }, ...results);
+              resolve(metadata);
+            })
+            .catch((e) => {
+              reject(e);
+            });
+        });
+      });
+
+      Promise.all(breadcrumbLoaders)
+        .then((results) => {
+          resolve({ breadcrumb: results });
+        })
+        .catch(reject);
     });
   }
 
@@ -317,6 +394,30 @@ class InPreviewEditingManager {
       } else {
         content.load().then(() => {
           resolve({ status: content.getLifecycleStatus() });
+        });
+      }
+    });
+  }
+
+  #loadSiteLocale(content: Content) {
+    return new Promise((resolve, reject) => {
+      if (!content) {
+        reject();
+      } else {
+        content.load().then(() => {
+          resolve({ siteLocale: SiteUtil.getSiteLocaleNameFor(content) });
+        });
+      }
+    });
+  }
+
+  #loadTranslationStatus(content: Content) {
+    return new Promise((resolve, reject) => {
+      if (!content) {
+        reject();
+      } else {
+        content.load().then(() => {
+          resolve({ translationStatus: TranslationStatusUtil.getTranslationStatus(content) });
         });
       }
     });
@@ -387,6 +488,40 @@ class InPreviewEditingManager {
     });
   }
 
+  #loadMetrics(content: Content) {
+    return new Promise((resolve, reject) => {
+      if (!content) {
+        reject();
+      } else {
+        // Metrics only available for channels and articles
+        if (!content.getType().isSubtypeOf("CMChannel") && !content.getType().isSubtypeOf("CMArticle")) {
+          reject();
+        }
+
+        const uuid = content.getUuid();
+        const siteService = editorContext._.getSitesService();
+        const site = siteService.getSiteFor(content);
+        const siteId = site.getId();
+        // console.log("[InPreviewEditingManager] Loading metrics for content UUID: ", uuid);
+        const metricsUrl = `cmec/proxy/phoenix/site/${siteId}/metrics/pages-details/${uuid}?datetime=last30days`;
+        // console.log("Metrics URL: ", metricsUrl);
+
+        fetchFromRemoteService(metricsUrl).then((response) => {
+          if (!response.ok) {
+            reject();
+          } else {
+            response
+              .json()
+              .then((json) => {
+                resolve({ metrics: json.data });
+              })
+              .catch(reject);
+          }
+        });
+      }
+    });
+  }
+
   #sendContentMetadataResponse(metadata: any): void {
     const contentWindow = this.#previewIframe.getContentWindow();
     const data = {
@@ -394,6 +529,16 @@ class InPreviewEditingManager {
     };
     console.log("[InPreviewEditingManager] Sending content metadata response: ", data);
     messageService.sendMessage(contentWindow, InPreviewEditingManager.MESSAGE_TYPE_CONTENT_METADATA_RESPONSE, data);
+  }
+
+  #sendContentMetricsResponse(metricsData: any): void {
+    const contentWindow = this.#previewIframe.getContentWindow();
+    console.log("[InPreviewEditingManager] Sending content metrics response: ", metricsData);
+    messageService.sendMessage(
+      contentWindow,
+      InPreviewEditingManager.MESSAGE_TYPE_CONTENT_METRICS_RESPONSE,
+      metricsData,
+    );
   }
 
   getFloatingEditorDialog(): FloatingEditorDialog {
